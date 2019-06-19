a63aeb1bd8aef61087e7f30d0ed846a2893fcdc5
Ismar Rodrigues da Silva <ismar.rodrigues@labs.dcx>
2019-06-19 12:43:37 -0300

    Adicionado pack de zoológico

---
+++ Zoo/Cachorro.java
@@ -0,0 +1,10 @@
+public class Cachorro extends Animal {
+	
+	public void emitirSom() {
+		System.out.println("Chorrrroooo");
+	}
+	
+	public void correr() {
+		System.out.println("Corre cachorrrroooo!");
+	}
+}
---
+++ Zoo/Cavalo.java
@@ -0,0 +1,10 @@
+public class Cavalo extends Animal {
+
+	public void emitirSom() {
+		System.out.println("Cavaaaaalooooo");
+	}
+	
+	public void correr() {
+		System.out.println("Corre cavaaaaalooooo");
+	}
+}
