858242603931a9a94ff5334c479c25d0ead7574e
Ismar Rodrigues <ismarrodrigues13@gmail.com>
2019-06-23 19:30:44 -0300

    Ultimas modificações

--- Interfaces/Quadrado.java
+++ Interfaces/Quadrado.java
@@ -6,7 +6,7 @@ public class Quadrado implements AreaCalculavel {
 		this.lado = lado;
 	}
 	public String toString(){
-		return "O quadrado de lado "+this.lado+" tem area "+this.lado*this.lado;
+		return "O quadrado de lado "+this.lado+" tem area "+this.lado*this.lado+".";
 	}
 	public double calcularArea(){
 		return this.lado*this.lado;
